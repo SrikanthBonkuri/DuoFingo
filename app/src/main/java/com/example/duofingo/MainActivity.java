f923a04f95f846f37c0f74fb6807676e8f9f9e13
ayushdj <ayushdj13@gmail.com>
2022-07-31 18:14:25 -0400

    added location services

--- app/src/main/java/com/example/duofingo/MainActivity.java
+++ app/src/main/java/com/example/duofingo/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openLoginSignUp() {
-        Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+        Intent intent = new Intent(MainActivity.this, LoginSignUp.class);
         startActivity(intent);
     }
 }
