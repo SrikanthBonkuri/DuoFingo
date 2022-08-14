cde823eccc12f494f55811044eef005fa62c816a
Shreyas <shreyas.rkarthik@gmail.com>
2022-08-13 20:27:28 -0400

    Completed Landing Page.

--- app/src/main/java/com/example/duofingo/MainActivity.java
+++ app/src/main/java/com/example/duofingo/MainActivity.java
@@ -3,16 +3,35 @@ package com.example.duofingo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button getStarted;
+    TextView catchPhrase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        catchPhrase = findViewById(R.id.catch_phrase);
+
+        String[] catchPhrases = {"World's best way to learn finance", "Learn and Compete",
+                "Quick and fun lessons that work"};
+        catchPhrase.post(new Runnable() {
+            int i = 0;
+            @Override
+            public void run() {
+                catchPhrase.setText(catchPhrases[i]);
+                i++;
+                if (i == catchPhrases.length)
+                    i = 0;
+                catchPhrase.postDelayed(this, 5000);
+            }
+        });
 
         getStarted = findViewById(R.id.get_started_button);
         getStarted.setOnClickListener(v -> openLoginSignUp());
